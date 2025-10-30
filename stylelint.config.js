7ab8f4f917654ef6f8285d1d29c2dc9233b4c57c
Andrew Dyer <andrewdyer@outlook.com>
2025-10-30 19:26:21 +0000

    chore: :wrench: add stylelint configuration file
    Introduce a new stylelint configuration to enforce consistent styling rules for SCSS files.

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+/** @type {import("stylelint").Config} */
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
+  rules: {
+    indentation: 2,
+    'max-nesting-depth': 3,
+    'color-hex-length': 'short',
+    'scss/dollar-variable-pattern': '^[_a-z0-9-]+$',
+    'scss/percent-placeholder-pattern': '^[_a-z0-9-]+$',
+    'selector-class-pattern': '^[a-z0-9\\-]+$'
+  }
+};
